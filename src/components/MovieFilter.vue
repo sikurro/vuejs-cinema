8c0ee1325091425a60d7768eb05e60103bc9f6c1
sikurro <sikurro@gmail.com>
2018-06-25 10:48:08 +0700

    filter session course | before emit, bus

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="movie-filter">
+        <h2>Filter Results</h2>
+        <h3>By time of day</h3>
+        <div class="filter-group">
+            <check-filter 
+              v-for="time in times" 
+              v-bind:key="time"
+              v-bind:title="time"
+              v-on:check-filter="checkFilter"
+              category="time"
+            ></check-filter>
+        </div>
+        <h3>By genre</h3>
+        <div class="filter-group">
+            <check-filter 
+              v-for="genre in genres" 
+              v-bind:key="genre"
+              v-bind:title="genre"
+              v-on:check-filter="checkFilter"
+              category="genre"
+            ></check-filter>
+        </div>
+    </div>
+</template>
+<script>
+import genres from "../util/genres";
+import times from '../util/times'
+import CheckFilter from './CheckFilter.vue';
+
+export default {
+  data() {
+    return {
+      genres,
+      times
+    };
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      this.$emit("check-filter", category, title, checked);
+    }
+  },
+  components: {
+    CheckFilter,
+  }
+};
+</script>
